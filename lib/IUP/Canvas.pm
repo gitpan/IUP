639c02616fcbb79cdf43f768fe6966cc6a24c496
kmx <kmx@cpan.org>
2011-08-17 20:50:54 +0000

    Import of KMX/IUP-0.002_12 from CPAN.
    gitpan-cpan-distribution: IUP
    gitpan-cpan-version:      0.002_12
    gitpan-cpan-path:         KMX/IUP-0.002_12.tar.gz
    gitpan-cpan-author:       KMX
    gitpan-cpan-maturity:     developer

--- lib/IUP/Canvas.pm
+++ lib/IUP/Canvas.pm
@@ -31,7 +31,5 @@ package IUP::Canvas::Bitmap;
 package IUP::Canvas::Palette;
 package IUP::Canvas::Pattern;
 package IUP::Canvas::Stipple;
-package IUP::Canvas::InternalServerImage;
-package IUP::Canvas::InternalState;
 
 1;
